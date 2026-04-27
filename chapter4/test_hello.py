7b91d5352f85115744a80bb4d1517ade84f65186
mahameddalmar20034-cpu <mahameddalmar20034@gmail.com>
2026-04-28 00:42:00 +0100

    combined matrices pipeline-v4

--- chapter4/test_hello.py
+++ chapter4/test_hello.py
@@ -2,4 +2,4 @@ import unittest
 from hello import say_hello
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual(say_hello(), "Hello, World!")
+        self.assertEqual(say_hello(), "Hello World!")
